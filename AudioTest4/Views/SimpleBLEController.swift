f95f77afebb3a3cde95a78c6671de6fe6b9abf2a
Perry Fraser <perry@frasers.org>
2017-01-09 18:33:22 -0500

    Added VC.
    I added a view controller which is render under Other Things -> Bluetooth -> Basic BLE. The respective view controller is under Views/BasicBLEController
    
    @sfraser please add your BT stuff to that file.

---
+++ AudioTest4/Views/SimpleBLEController.swift
@@ -0,0 +1,38 @@
+//
+//  SimpleBLEController.swift
+//  AudioTest4
+//
+//  Created by Perry Fraser on 1/9/17.
+//  Copyright © 2017 Scott Fraser. All rights reserved.
+//
+
+import UIKit
+
+class SimpleBLEController: UIViewController {
+
+    // @sfraser add all your BT stuff here. Treat it like a normal ViewController (cause it is)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
