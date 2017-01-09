9efd6428e55494f0052a898f36980fbabb83cc97
Scott Fraser <scott@frasers.org>
2017-01-08 22:16:59 -0500

    Got beacons reading from iOS - have to run this on a real device. Was able to monitor and range the beacon! Right now just dumping information to console.

--- AudioTest4/ViewController.swift
+++ AudioTest4/ViewController.swift
@@ -28,11 +28,16 @@ class ViewController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         locationManager.delegate = self
         
+        // @todo I AM HERE - why is this Failed monitoring region Optional("test beacon"): The operation couldn’t be completed. (kCLErrorDomain error 5.)
+
+        
         // just for testing - hard coded start of monitoring a beacon
         startMonitoringItem(item: BeaconInfo(name: "test beacon",
-                                             uuid: UUID( uuidString: "E621E1F8-C36C-495A-93FC-0C247A3E6E5F")!,
-                                                majorValue: 123,
-                                                minorValue: 456))
+                                             // this is uuid and major/minor of example RFDuino programmed to range:
+                                             uuid: UUID( uuidString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0")!,
+                                                majorValue: 1234,
+                                                minorValue: 5678))
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -63,8 +68,8 @@ extension ViewController : CLLocationManagerDelegate {
     
     private func beaconRegionWithItem(item:BeaconInfo) -> CLBeaconRegion {
         let beaconRegion = CLBeaconRegion(proximityUUID: item.uuid,
-                                          major: item.majorValue,
-                                          minor: item.minorValue,
+                                          //major: item.majorValue,
+                                          //minor: item.minorValue,
                                           identifier: item.name)
         return beaconRegion
     }
@@ -73,16 +78,18 @@ extension ViewController : CLLocationManagerDelegate {
         let beaconRegion = beaconRegionWithItem(item: item)
         locationManager.startMonitoring(for: beaconRegion)
         locationManager.startRangingBeacons(in: beaconRegion)
+        print(">>> Started monitoring!!!")
     }
     
     func stopMonitoringItem(item: BeaconInfo) {
         let beaconRegion = beaconRegionWithItem(item: item)
         locationManager.stopMonitoring(for: beaconRegion)
         locationManager.stopRangingBeacons(in: beaconRegion)
+        print(">>> STOPPED monitoring!!!")
     }
     
     func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
-        print("Failed monitoring region: \(error.localizedDescription)")
+        print("Failed monitoring region \(region?.identifier): \(error.localizedDescription)")
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -90,11 +97,33 @@ extension ViewController : CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+            // print(">>> didRangeBeacons in \(region.proximityUUID)")
             for beacon in beacons {
+                
+                print("FOUND BEACON: \(beacon.proximityUUID) \(nameForProximity(proximity: beacon.proximity)) rssi:\(beacon.rssi))")
                 
                 // see what's up with the beacons in range...
                 // @todo I AM HERE - https://www.raywenderlich.com/101891/ibeacons-tutorial-ios-swift
         }
     }
     
+}
+
+func ==(item: BeaconInfo, beacon: CLBeacon) -> Bool {
+    return ((beacon.proximityUUID.uuidString == item.uuid.uuidString)
+        && (Int(beacon.major) == Int(item.majorValue))
+        && (Int(beacon.minor) == Int(item.minorValue)))
+}
+
+func nameForProximity(proximity: CLProximity) -> String {
+    switch proximity {
+    case .unknown:
+        return "Unknown"
+    case .immediate:
+        return "Immediate"
+    case .near:
+        return "Near"
+    case .far:
+        return "Far"
+    }
 }
